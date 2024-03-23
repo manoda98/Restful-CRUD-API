9a8f7a83e2a1060f00188e61b01a9bcd60e14296
manoda98 <2020e147@eng.jfn.ac.lk>
2024-03-23 15:42:17 +0530

    Update product in  the database

--- server.js
+++ server.js
@@ -15,25 +15,6 @@ app.get('/blog' , (req , res) => {
     res.send('Hello Blog my name is manoda')
 })
 
-app.get('/products', async(req , res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: ErrorEvent.message})
-    }
-})
-
-app.get('/products/:id', async(req,res) => {
-    try {
-        const {id} = req.params;
-        const products = await Product.findById({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: ErrorEvent.message})
-    }
-})
-
  app.post('/products',async(req,res) => {
     try {
         console.log(req.body)
@@ -45,6 +26,43 @@ app.get('/products/:id', async(req,res) => {
     }
  })
 
+ app.get('/products', async(req , res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: ErrorEvent.message})
+    }
+})
+
+ app.get('/products/:id', async(req,res) => {
+    try {
+        const {id} = req.params;
+        console.log(id)
+        const products = await Product.findById(id);
+        //const products = await Product.findById(new mongoose.Types.ObjectId(id));
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//update a product
+
+app.put('/products/:id', async(req,res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id,req.body);
+        //cannot find any product in database
+        if(!product){
+            return res.status(404).json({message:`cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.set("strictQuery" , false)
 
