fb103cbc904040d44293bd0e07c984f5005165f3
manoda98 <2020e147@eng.jfn.ac.lk>
2024-03-23 15:56:16 +0530

    put products to th database using form encoded

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Product =  require('./models/productModel')
+const e = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //routes
 
@@ -57,7 +59,8 @@ app.put('/products/:id', async(req,res) => {
         if(!product){
             return res.status(404).json({message:`cannot find any product with ID ${id}`})
         }
-        res.status(200).json(product);
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
 
     } catch (error) {
         res.status(500).json({message: error.message})
