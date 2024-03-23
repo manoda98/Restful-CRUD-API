093c75a6e8ee59b3d7dfcecca1ba491f0849f457
manoda98 <2020e147@eng.jfn.ac.lk>
2024-03-23 11:35:47 +0530

    implement create product endpoint

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Product =  require('./models/productModel')
 const app = express()
+
+app.use(express.json())
 
 //routes
 
@@ -7,6 +11,30 @@ app.get('/' , (req , res) => {
     res.send('Hello NODE API')
 })
 
-app.listen(3000 , ()=> {
-    console.log('node API app is running on port 3000')
+app.get('/blog' , (req , res) => {
+    res.send('Hello Blog my name is manoda')
+})
+ app.post('/product',async(req,res) => {
+    try {
+        console.log(req.body)
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    }catch (error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+ })
+
+
+mongoose.set("strictQuery" , false)
+
+mongoose.connect('mongodb+srv://manoda:Imedha2003@cluster0.wqw0c66.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000 , ()=> {
+        console.log('node API app is running on port 3000')
+    })
+    
+}).catch((error) => {
+    console.log(error)
 })
